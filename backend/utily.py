55355c135003e10ff6b8dafa09c05d2be23aa920
sistemaschgw <sistemaschgw@gmail.com>
2026-07-30 14:55:58 -0600

    act

--- backend/utily.py
+++ backend/utily.py
@@ -118,7 +118,7 @@ def get_db_connection():
     return psycopg2.connect(
         host=os.getenv("POSTGRES_HOST", "postgres"),
         port=os.getenv("POSTGRES_PORT", "5432"),
-        database=os.getenv("POSTGRES_DB", "Rh-System"),
+        database=os.getenv("POSTGRES_DB", "rh-system"),
         user=os.getenv("POSTGRES_USER", "rh_app"),
         password=os.getenv("POSTGRES_PASSWORD", "S1s73m4s!"),
     )
