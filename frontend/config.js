b829516b7afd20a0b0dd4379bd848b07de18f051
sistemaschgw <sistemaschgw@gmail.com>
2026-08-07 11:34:06 -0600

    up

---
+++ frontend/config.js
@@ -0,0 +1,12 @@
+window.LOGIN_CONFIG = {
+  rh: {
+    username: 'RH',
+    password: '123',
+    redirect: 'iniciorh.html'
+  },
+  reclutamiento: {
+    username: 'Reclutamiento',
+    password: '321',
+    redirect: 'InicioReclu.html'
+  }
+};
